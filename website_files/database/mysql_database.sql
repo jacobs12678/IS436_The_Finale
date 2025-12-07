dabd804e7265e290c70715f8c8eaa5a5a1064ac1
Jacob Shim <jacobs12@umbc.edu>
2025-12-07 10:32:49 -0500

    new database

--- website_files/database/mysql_database.sql
+++ website_files/database/mysql_database.sql
@@ -1,101 +1,119 @@
 -- Creating Group A's database
-CREATE DATABASE main_project_db;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 /*
 Tables are created in the order shown below along with the inserted values.
 */
 /*----------------------------------------------------------------------------------------------*/
 
+/* input and delete required */
 /* Submit & Log Items */
-CREATE TABLE main_project_db.submitlog (
+CREATE TABLE submitlog (
 sl_id int NOT NULL AUTO_INCREMENT,
-item_name varchar(100),
-location varchar(100),
-item_type varchar(100),
-item_desc varchar(200),
+item_name varchar(100) NOT NULL,
+item_location varchar(100)  NOT NULL,
+item_type varchar(100)  NOT NULL,
+item_desc varchar(200)  NOT NULL,
 registered_item TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-CONSTRAINT submitlog_PK PRIMARY KEY (sl_id));
+CONSTRAINT submitlog_PK PRIMARY KEY (sl_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- INSERT VALUES for submitlog table
-INSERT INTO main_project_db.submitlog (sl_id, item_name, location, item_type, item_desc) VALUES
+INSERT INTO submitlog (sl_id, item_name, item_location, item_type, item_desc) VALUES
 (1, 'credit card', 'Commons', 'Identification', 'Light blue card with name and number');
-INSERT INTO main_project_db.submitlog (sl_id, item_name, location, item_type, item_desc) VALUES
+INSERT INTO submitlog (sl_id, item_name, item_location, item_type, item_desc) VALUES
 (2, 'social security card', 'Commons', 'Visa credit card with bank logo', 'Used for conducting digital transactions.');
-INSERT INTO main_project_db.submitlog (sl_id, item_name, location, item_type, item_desc) VALUES
+INSERT INTO submitlog (sl_id, item_name, item_location, item_type, item_desc) VALUES
 (3, 'house keys', 'Commons', 'Keys with a green keychain', 'Access to entering a home.');
+
+ALTER TABLE submitlog MODIFY sl_id int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 3;
 /*----------------------------------------------------------------------------------------------*/
 
+/* no input and delete required */
 /* Subscription Table */
-CREATE TABLE main_project_db.subscription (
+CREATE TABLE subscription (
 sub_id int NOT NULL AUTO_INCREMENT, 
-service varchar(300),
-cost int,
-payment_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+sub_service varchar(300)  NOT NULL,
+cost int  NOT NULL,
+payment_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  NOT NULL,
 CONSTRAINT sub_PK PRIMARY KEY (sub_id));
 
 -- INSERT VALUES for subscription table
-INSERT INTO main_project_db.subscription (sub_id, service, cost) VALUES
+INSERT INTO subscription (sub_id, sub_service, cost) VALUES
 (1, '3 Months Subscription Plan', 30);
-INSERT INTO main_project_db.subscription (sub_id, service, cost) VALUES
+INSERT INTO subscription (sub_id, sub_service, cost) VALUES
 (2, '6 Months Subscription Plan', 60);
-INSERT INTO main_project_db.subscription (sub_id, service, cost) VALUES
+INSERT INTO subscription (sub_id, sub_service, cost) VALUES
 (3, '1 Year Subscription Plan', 90);
 /*----------------------------------------------------------------------------------------------*/
 
+/* input and delete required */
 /* Ticket Table */
-CREATE TABLE main_project_db.ticket (
+CREATE TABLE ticket (
 t_id int NOT NULL AUTO_INCREMENT, 
-confirmation varchar(50),
-type varchar(50),
-submitted timestamp,
-CONSTRAINT t_PK PRIMARY KEY (t_id));
+confirmation varchar(50)  NOT NULL,
+ticket_type varchar(50)  NOT NULL,
+submitted timestamp  NOT NULL,
+CONSTRAINT t_PK PRIMARY KEY (t_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- INSERT VALUES for ticket table
-INSERT INTO main_project_db.ticket (t_id, confirmation, type, submitted) VALUES
+INSERT INTO ticket (t_id, confirmation, ticket_type, submitted) VALUES
 (1, 'Confirmed', 'Service Request', '2025-12-01 10:15:00');
-INSERT INTO main_project_db.ticket (t_id, confirmation, type, submitted) VALUES
+INSERT INTO ticket (t_id, confirmation, ticket_type, submitted) VALUES
 (2, 'Pending', 'Billing and Payment', '2025-12-01 11:34:25');
-INSERT INTO main_project_db.ticket (t_id, confirmation, type, submitted) VALUES
+INSERT INTO ticket (t_id, confirmation, ticket_type, submitted) VALUES
 (3, 'Cancelled', 'Technical Support', '2025-12-01 09:45:36');
+
+ALTER TABLE ticket MODIFY t_id int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 3;
 /*----------------------------------------------------------------------------------------------*/
 
+/* input and delete required */
 /* Login & Signup Table */
-CREATE TABLE main_project_db.loginsign (
+CREATE TABLE loginsign (
 ls_id int NOT NULL AUTO_INCREMENT,
-ls_t_id int,
-ls_sub_id int,
-user_name varchar(50),
-username varchar(50),
-password varchar(100),
-email varchar(100),
+ls_t_id int  NOT NULL,
+ls_sub_id int  NOT NULL,
+user_name varchar(50)  NOT NULL,
+username varchar(50)  NOT NULL,
+user_password varchar(100)  NOT NULL,
+email varchar(100)  NOT NULL,
 registered_item TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 CONSTRAINT loginsign_PK PRIMARY KEY (ls_id),
 CONSTRAINT t_FK FOREIGN KEY (ls_t_id) REFERENCES ticket (t_id),
-CONSTRAINT sub_FK FOREIGN KEY (ls_sub_id) REFERENCES subscription (sub_id));
+CONSTRAINT sub_FK FOREIGN KEY (ls_sub_id) REFERENCES subscription (sub_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- INSERT VALUES for loginsign table
-INSERT INTO main_project_db.loginsign (ls_id, ls_t_id, ls_sub_id, user_name, username, password, email) VALUES
+INSERT INTO loginsign (ls_id, ls_t_id, ls_sub_id, user_name, username, user_password, email) VALUES
 (1, 1, 3, 'Derp', 'derp123', 'sushiwushi11', 'derp25@umbc.edu');
-INSERT INTO main_project_db.loginsign (ls_id, ls_t_id, ls_sub_id, user_name, username, password, email) VALUES
+INSERT INTO loginsign (ls_id, ls_t_id, ls_sub_id, user_name, username, user_password, email) VALUES
 (2, 3, 2, 'Sophie', 'sophie456', 'pollens36', 'sophie21@umbc.edu');
-INSERT INTO main_project_db.loginsign (ls_id, ls_t_id, ls_sub_id, user_name, username, password, email) VALUES
+INSERT INTO loginsign (ls_id, ls_t_id, ls_sub_id, user_name, username, user_password, email) VALUES
 (3, 2, 1, 'Link', 'link789', 'triforce3', 'link33@umbc.edu');
+
+ALTER TABLE loginsign MODIFY ls_id int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 3;
 /*----------------------------------------------------------------------------------------------*/
 
+/* input and delete required */
 /* Search Function/Display General Item(s) */
-CREATE TABLE main_project_db.searchdisplay (
+CREATE TABLE searchdisplay (
 sd_id int NOT NULL AUTO_INCREMENT,
-sl_id_FK int,
-ls_id_FK int,
-keyword varchar(100),
+sl_id_FK int  NOT NULL,
+ls_id_FK int  NOT NULL,
+keyword varchar(100)  NOT NULL,
 CONSTRAINT searchdisplay_PK PRIMARY KEY (sd_id),
 CONSTRAINT loginsign_FK FOREIGN KEY (ls_id_FK) REFERENCES loginsign (ls_id),
-CONSTRAINT submitlog_FK FOREIGN KEY (sl_id_FK) REFERENCES submitlog (sl_id));
+CONSTRAINT submitlog_FK FOREIGN KEY (sl_id_FK) REFERENCES submitlog (sl_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- INSERT VALUES for searchdisplay table
-INSERT INTO main_project_db.searchdisplay (sd_id, sl_id_FK, ls_id_FK, keyword) VALUES
+INSERT INTO searchdisplay (sd_id, sl_id_FK, ls_id_FK, keyword) VALUES
 (1, 2, 1, 'credit card');
-INSERT INTO main_project_db.searchdisplay (sd_id, sl_id_FK, ls_id_FK, keyword) VALUES
+INSERT INTO searchdisplay (sd_id, sl_id_FK, ls_id_FK, keyword) VALUES
 (2, 1, 3, 'social security card');
-INSERT INTO main_project_db.searchdisplay (sd_id, sl_id_FK, ls_id_FK, keyword) VALUES
+INSERT INTO searchdisplay (sd_id, sl_id_FK, ls_id_FK, keyword) VALUES
 (3, 3, 2, 'house keys');
+
+ALTER TABLE searchdisplay MODIFY sd_id int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 3;
 /*----------------------------------------------------------------------------------------------*/
+
+COMMIT;
