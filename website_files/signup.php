cbf7d8ebccecc4515d5b9eb5825a67e8b8cfedd5
brianpark4 <bpark4@umbc.edu>
2025-12-07 23:41:08 -0500

    awdasdad

--- website_files/signup.php
+++ website_files/signup.php
@@ -14,7 +14,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         $username = $_POST["username"];
         $password = $_POST["password"];
 
-        $insert = "INSERT INTO loginsign (user_name, username, user_password, email) VALUES ($name, $username, $password, $email)";
+        $insert = "INSERT INTO loginsign (user_name, username, user_password, email) VALUES ('$name', '$username', '$password', '$email')";
 
         if ($conn->query($insert) === TRUE){
             echo "Insert Successfull";
