c247bfc2d2d150f9b0093d2beff3b1f76dfdcc89
brianpark4 <bpark4@umbc.edu>
2025-12-07 19:47:43 -0500

    yes

--- website_files/login.php
+++ website_files/login.php
@@ -4,14 +4,18 @@ session_start();
 if($_SERVER["REQUEST_METHOD"] = "POST") {
   $conn = mysqli('db', 'UMBCstudent', 'bongocat123', 'main_project_db');
 
+  if ($conn->connect_error) {
+    die("Connection failed: " . htmlspecialchars($conn->connect_error));
+  }
+
   $username = $_POST["username"];
   $password = $_POST["password"];
+  $_SESSION['loggedin'] = true;
 
   print($username);
   print($password);
 
 }
-
 
 ?>
 
