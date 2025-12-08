54767c53c7134ad65b6303a5202464f56b1a9892
brianpark4 <bpark4@umbc.edu>
2025-12-07 22:30:11 -0500

    asdasd

--- website_files/index.php
+++ website_files/index.php
@@ -2,69 +2,76 @@
 session_start();
 echo ($_SESSION["username"]);
 if (!isset($_SESSION["logged"])) {
-	header('Location: login.php');
-	exit();
+   header('Location: login.php');
+   exit();
 }
 ?>
 
+
 <!doctype html>
 <html lang="en">
-    <head>
-        <title> UMBC Lost & Found Home Page </title>
+   <head>
+       <title> UMBC Lost & Found Home Page </title>
 
-        <!-- Style -->
-        <style>
-        	h2 {
-        	  font-size: 35px;
-        	}
-        	table, th, td{
-        	  text-align: center;
-			  padding: 0 24px;
-			  margin: 0 24px;
-			  font-size: 20px;
-        	}
-        	.formathead {
-        	  color: orange;
-        	  font-weight: bold;
-        	  text-align: left;
-        	  display: flex;
-        	  margin: 0 20px;
-        	  padding: 0 20px;
-        	  font-size: 25px;
-        	}
-			.centered {
-				text-align: center;
-			}
-			.color {
-				text-weight: bold;
-				color: orange;
-			}
-    	</style>
-    </head>
-    
-    <body>
-        <!-- The Homepage Title with Other Sections -->
-		<h1> WELCOME TO THE LOST & FOUND </h1>
-		<p> A place for UMBC members to report and find their missing belongings in a time of crisis.</p>
 
-		<!-- Image -->
-		<p class="centered"> <img src="pngwing.com(1).png" alt="main picture" width="500" height="300"> </p>
-		<!-- The 3 Links -->
-		 <div>
-			<div>
-				<table>
-					<thead>
-						<tr>
-							<th><a href="forms/report_dashboard.html" class="color"> Report <img src="report_icon.jpg" alt="report" width="150"></a></th>
-				            
-							<th><a href="search.php" class="color"> Search <img src="search_icon.png" alt="search" width="155"></a></th>
-						
-							<th><a href="forms/admin_dashboard.html" class="color"> Tickets <img src="lost_item_icon.png" alt="lostitem" width="160"></a></th>
-						</tr>
-					</thead>
-				</table>
-		    </div>
-		 </div>
-    </body>
+       <!-- Style -->
+       <style>
+           h2 {
+             font-size: 35px;
+           }
+           table, th, td{
+             text-align: center;
+             padding: 0 24px;
+             margin: 0 24px;
+             font-size: 20px;
+           }
+           .formathead {
+             color: orange;
+             font-weight: bold;
+             text-align: left;
+             display: flex;
+             margin: 0 20px;
+             padding: 0 20px;
+             font-size: 25px;
+           }
+           .centered {
+               text-align: center;
+           }
+           .color {
+               text-weight: bold;
+               color: orange;
+           }
+       </style>
+   </head>
+  
+   <body>
+       <!-- The Homepage Title with Other Sections -->
+       <h1> WELCOME TO THE LOST & FOUND </h1>
+       <p> A place for UMBC members to report and find their missing belongings in a time of crisis.</p>
+
+
+       <!-- Image -->
+       <p class="centered"> <img src="pngwing.com(1).png" alt="main picture" width="500" height="300"> </p>
+       <!-- The 4 Links -->
+        <div>
+           <div>
+               <table>
+                   <thead>
+                       <tr>
+                           <th class="color"> Report <img src="report_icon.jpg" alt="report" width="150" height="150"> </th>
+                          
+                           <th class="color"> Search <img src="search_icon.png" alt="search" width="160" height="160"> </th>
+                      
+                           <th class="color"> Find <img src="lost_item_icon.png" alt="lostitem" width="170" height="170"> </th>
+                           <!-- Ticket needs an image -->
+                           <th> <a href="forms/admin_dashboard.html" class="color"> Tickets </a></th>
+                           <th> <a href="logout.php">Logout</a></th>
+
+
+                       </tr>
+                   </thead>
+               </table>
+           </div>
+        </div>
+   </body>
 </html>
-
