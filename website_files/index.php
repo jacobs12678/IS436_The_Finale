f332adbdb01b02f5b5492689a2de399132afa259
Jacob Shim <jacobs12@umbc.edu>
2025-12-09 08:20:34 -0500

    nope

--- website_files/index.php
+++ website_files/index.php
@@ -29,7 +29,6 @@ if (!isset($_SESSION["logged"])) {
 			  font-weight: bold;
            }
            table, th, td{
-             align: center;
         	 text-align: center;
              padding: 0 24px;
              margin: 0 24px;
